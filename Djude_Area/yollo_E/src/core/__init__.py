d767c105f9f48e5e254b1f856b4579b35680a266
SmallLoOwO <puppy100661@gmail.com>
2026-03-24 13:21:13 +0800

    feat: create core module structure

---
+++ Djude_Area/yollo_E/src/core/__init__.py
@@ -0,0 +1,6 @@
+"""核心共用模組"""
+
+from .detection_engine import DetectionEngine, DetectionConfig
+from .data_manager import DataManager
+
+__all__ = ["DetectionEngine", "DetectionConfig", "DataManager"]
